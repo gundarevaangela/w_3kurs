855e368b89a81db24771415b211f4645952f55e8
Gundareva Angela <gundarevaangela9@gmail.com>
2025-09-11 23:39:52 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start():
+
+@app.route("/web")
+def web():
     return """<!doctype html> 
         <html> 
            <body> 
                <h1>web-сервер на flask<h1> 
+               <a href="/author">author</a>
            </body> 
         </html>"""    
+@app.route("/author")
+def author():
+        name = "Gundareva Angela"
+        group ="FBI-31"
+        faculty = "FB"
+        return """<!doctype html>
+            <html>
+                <body>
+                    <p> Студент: """ + name + """</p>
+                    <p> Группа: """ + group + """</p>
+                    <p> Факультет: """ + faculty + """</p>
+                    <a href="/web">web</a>
+                </body>
+            </html>""" 
