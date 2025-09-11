f01f50a8352db9059d58f3b661a58c1e77b62083
Gundareva Angela <gundarevaangela9@gmail.com>
2025-09-12 00:40:25 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 import datetime
 app = Flask(__name__)
 
@@ -60,3 +60,7 @@ def counter():
     </body>
 </html>
 '''
+
+@app.route("/info")
+def info():
+      return redirect("/author")
