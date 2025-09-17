706950ed4ff47efec638963f47958c592840bc3e
Студент Факультета Бизнеса <student@fb.local>
2025-09-17 18:13:25 +0700

    Имя студента

--- app.py
+++ app.py
@@ -565,4 +565,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Gundareva Angela'
+    return render_template('example.html', name=name)
