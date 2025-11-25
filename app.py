4b2caa0633f9129b8171d59198380e3e13c1ade7
Gundareva Angela <gundarevaangela9@gmail.com>
2025-11-25 16:46:38 +0700

    Заготовка к 6 лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 
@@ -16,6 +17,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 
 # список логов
@@ -308,6 +310,7 @@ def index():
             <a href="/lab3">Третья лабораторная</a><br>
             <a href="/lab4">Четвертая лабораторная</a><br>
             <a href="/lab5">Пятая лабораторная</a><br>
+            <a href="/lab6">Шестая лабораторная</a><br>
         </nav>
         <footer>
             Гундарева Анжела, ФБИ-31, 3 курс, 2025
